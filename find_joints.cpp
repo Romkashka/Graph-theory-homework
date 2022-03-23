67cb09879a4f2cbb36bc1f054a05580ea0a66f70
Romkashka <halilov.g.2020@gmail.com>
2022-03-23 11:24:23 +0300

    Fix find_joints.cpp

--- find_joints.cpp
+++ find_joints.cpp
@@ -12,33 +12,37 @@ set<int> vertex_list[50], result, all;
 set<int> edges;
 map<pair<int, int>, int> vertex_id;
 map<int, pair<int, int>> vertex_name;
-int number, edge_number;
+int number, edge_number, components, components_init, visited[50];
 
-bool is_connected(int erased) {
-    erased = (erased + 1) % number;
-    int visited[50];
-    for (int i = 1; i < number; i++) {
-        visited[i] = 0;
-    }
+void bfs(int start, int color) {
     queue<int> q;
-    q.push(erased);
-    visited[erased] = 1;
+    q.push(start);
+    visited[start] = color;
     while (!q.empty()) {
         int current = q.front();
         q.pop();
         for (auto i: vertex_list[current]) {
-            if (!visited[i]) {
+            if (visited[i] == -1) {
                 q.push(i);
-                visited[i] = 1;
+                visited[i] = color;
             }
         }
     }
+}
+
+bool is_connected(int erased) {
     for (int i = 1; i < number; i++) {
-        if (!visited[i]) {
-            return false;
+        visited[i] = -1;
+    }
+    components = 0;
+    for (int i = 1; i < number; i++) {
+        if (i != erased && visited[i] == -1) {
+            bfs(i, components);
+            components++;
         }
     }
-    return true;
+    cerr << "components: " << components << endl;
+    return components <= components_init;
 }
 
 void print(int i) {
@@ -50,7 +54,7 @@ int main() {
     freopen("joints.out", "w", stdout);
     string x, y;
     number = 1, edge_number = 1;
-    for (int i = 1; i <= 92; i++) {
+    for (int i = 1; i <= 93; i++) {
         cin >> x >> y;
         if (!get_id.count(x)) {
             get_id.insert({x, number});
@@ -71,6 +75,17 @@ int main() {
         vertex_list[get_id[x]].insert(get_id[y]);
         vertex_list[get_id[y]].insert(get_id[x]);
     }
+    components_init = 0;
+    for (int i = 1; i < number; i++) {
+        visited[i] = -1;
+    }
+    for (int i = 1; i < number; i++) {
+        if (visited[i] == -1) {
+            bfs(i, components_init);
+            components_init++;
+        }
+    }
+    cerr << components_init << endl;
     int a, b;
     for (auto current: all) {
         set<int> cpy = vertex_list[current];
@@ -78,6 +93,7 @@ int main() {
         for (auto i: cpy) {
             vertex_list[i].erase(current);
         }
+        cerr << get_name[current] << " ";
         if (!is_connected(current)) {
             result.insert(current);
         }
@@ -88,6 +104,6 @@ int main() {
     }
 
     for (auto i: result) {
-        cout << get_name[i] << " ";
+        cout << get_name[i] << endl;
     }
 }
