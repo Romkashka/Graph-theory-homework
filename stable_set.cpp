46f0bdb0916888aefc84b4d7e86424ad622e7597
Romkashka <halilov.g.2020@gmail.com>
2022-03-30 13:46:56 +0300

    [Bug]: fix stable_set.cpp

--- stable_set.cpp
+++ stable_set.cpp
@@ -10,21 +10,26 @@ map<string, int> get_id;
 map<int, string> get_name;
 set<int> vertex_list[50], result, all;
 
-void branch_and_bound(set<int> current, set<int> candidate) {
-    while (!candidate.empty()) {
-        auto vertex = candidate.begin();
-        set<int> cleaned({});
-        set<int> tmp({*vertex});
-        set_difference(candidate.begin(), candidate.end(),
-                       vertex_list[*vertex].begin(), vertex_list[*vertex].end(),
-                       inserter(cleaned, cleaned.begin()));
-        cleaned.erase(*vertex);
-        if (current.size() + cleaned.size() > result.size()) {
-            current.insert(*vertex);
-            branch_and_bound(current, cleaned);
-            current.erase(*vertex);
+set<int> diff(set<int> A, set<int> B) {
+    set<int> diff;
+    for (auto i: A) {
+        if (!B.count(i)) {
+            diff.insert(i);
         }
-        candidate.erase(*vertex);
+    }
+    return diff;
+}
+
+void branch_and_bound(set<int> current, set<int> candidate) {
+    for (auto i: candidate) {
+        set<int> cleaned = diff(candidate, vertex_list[i]);
+        cleaned.erase(i);
+        if (current.size() + cleaned.size() >= result.size()) {
+            current.insert(i);
+            branch_and_bound(current, cleaned);
+            current.erase(i);
+        }
+        candidate.erase(i);
     }
     if (current.size() > result.size()) {
         result = current;
@@ -36,7 +41,7 @@ int main() {
     freopen("graph.in", "r", stdin);
     freopen("stable_set.out", "w", stdout);
     string x, y;
-    int number = 1;
+    int number = 0;
     for (int i = 1; i <= 92; i++) {
         cin >> x >> y;
         if (!get_id.count(x)) {
@@ -52,7 +57,7 @@ int main() {
         vertex_list[get_id[x]].insert(get_id[y]);
         vertex_list[get_id[y]].insert(get_id[x]);
     }
-    for (int i = 1; i < number; i++) {
+    for (int i = 0; i < number; i++) {
         all.insert(i);
     }
     branch_and_bound(set<int>(), all);
